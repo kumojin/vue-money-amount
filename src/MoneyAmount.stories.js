b99be7c211e805989052e8bb4bd6ae2f0eeb14bb
meliecool <35310834+meliecool@users.noreply.github.com>
2020-10-26 13:05:14 -0400

    Add money amount story

---
+++ src/MoneyAmount.stories.js
@@ -0,0 +1,35 @@
+import MoneyAmount from './MoneyAmount';
+
+export default {
+  title: 'MoneyAmount',
+};
+
+export const USDAmount = () => ({
+  components: { MoneyAmount },
+  data: () => ({
+    amount: 4284,
+    currency: 'USD',
+    locale: 'en-US'
+  }),
+  template: `<money-amount :amount="amount" :currency="currency" :locale="locale" />`
+});
+
+export const CADAmount = () => ({
+  components: { MoneyAmount },
+  data: () => ({
+    amount: 4284,
+    currency: 'CAD',
+    locale: 'en-US'
+  }),
+  template: `<money-amount :amount="amount" :currency="currency" :locale="locale" />`
+});
+
+export const JPYAmount = () => ({
+  components: { MoneyAmount },
+  data: () => ({
+    amount: 4284,
+    currency: 'JPY',
+    locale: 'en-US'
+  }),
+  template: `<money-amount :amount="amount" :currency="currency" :locale="locale" />`
+});
