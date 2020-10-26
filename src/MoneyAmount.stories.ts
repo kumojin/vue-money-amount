8151a50560c17e65d574e8b33ba8219bf1a68a91
meliecool <35310834+meliecool@users.noreply.github.com>
2020-10-26 17:34:47 -0400

    Update tests + delete addons

--- src/MoneyAmount.stories.ts
+++ src/MoneyAmount.stories.ts
@@ -14,6 +14,16 @@ export const USDAmount = () => ({
   template: '<money-amount :amount="amount" :currency="currency" :locale="locale" />',
 });
 
+export const CADAmount = () => ({
+  components: { MoneyAmount },
+  data: () => ({
+    amount: 4284,
+    currency: 'CAD',
+    locale: 'fr-CA',
+  }),
+  template: '<money-amount :amount="amount" :currency="currency" :locale="locale" />',
+});
+
 export const JPYAmount = () => ({
   components: { MoneyAmount },
   data: () => ({
@@ -58,11 +68,12 @@ export const CADZero = () => ({
   components: { MoneyAmount },
   data: () => ({
     amount: 0,
-    currency: 'CAD',
-    locale: 'fr-CA',
+    currency: 'USD',
+    locale: 'en-US',
   }),
   template: '<money-amount :amount="amount" :currency="currency" :locale="locale" />',
 });
+
 export const withoutCurrency = () => ({
   components: { MoneyAmount },
   data: () => ({
