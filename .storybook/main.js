7163563708c10a69bb0b28a9cf745f128ade171f
Faustine Mastrodicasa <faustine@kumojin.com>
2021-10-18 20:03:31 -0400

    reinstall storybook with last version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,30 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.[tj]s'],
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, '../src/'),
-    }
-
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      exclude: /node_modules/,
-      loader: ['babel-loader', 'awesome-typescript-loader'],
-      include:  path.resolve(__dirname, '../'),
-    });
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
+  ],
+  features: {
+    postcss: false,
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
 
     config.module.rules.push(
       {
@@ -34,4 +41,4 @@ module.exports = {
     // Return the altered config
     return config;
   },
-};
+}
